a0e059f5ffbea4cd7203209330377fcaf71a785b
matheus <matheus.fu.p@hotmail.com>
2023-09-14 11:03:01 -0300

    add permissoes a methods

--- src/main/java/com/recados/listagem/config/WebConfig.java
+++ src/main/java/com/recados/listagem/config/WebConfig.java
@@ -1,15 +1,21 @@
 package com.recados.listagem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.function.ServerRequest;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+class WebConfig implements WebMvcConfigurer {
 
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
     }
 }
